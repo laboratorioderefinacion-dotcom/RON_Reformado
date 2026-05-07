9ebd92b7f5da84fe76f5064504bc109c994f80a3
laboratorioderefinacion-dotcom <laboratorioderefinacion@gmail.com>
2026-05-06 23:09:27 -0300

    Add files via upload

--- RON_Reformado_Streamlit.py
+++ RON_Reformado_Streamlit.py
@@ -171,7 +171,7 @@ if archivo is not None:
                 icono = "🟢"
 
             elif ron_std < UMBRAL_METODO_SUP:
-                color = "orange"
+                color = "yellow"
                 estado = "CONFIABILIDAD MEDIA"
                 icono = "🟡"
 
